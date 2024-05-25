42110e0ed58de655414b4c990730a482f6964b01
Ritik Gupta <guptaritik2203@gmail.com>
2024-05-25 00:15:56 -0400

    Color Changer javascript Dom Practice Project

---
+++ DOM based JS Projects/ColorChanger/script.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll(".button")
+const body= document.querySelector("body")
+
+console.log(body)
+buttons.forEach((button)=>{
+    console.log(button)
+    button.addEventListener('click',(e)=>{
+        console.log(e)
+        console.log(e.target)
+        body.style.backgroundColor = e.target.id;
+    })
+})
+
+
+
